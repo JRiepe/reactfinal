b28b538fa28361b918d6855fc3d3977bb3152500
John Riepe <johnriepe@gmail.com>
2016-09-24 18:02:29 -0400

    no home

--- app/Home.js
+++ app/Home.js
@@ -2,14 +2,11 @@ import React from 'react';
 import styles from './App.css';
 
 export default class Home extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {test: 'foo'};
-  }
+  
   render() {
     return (
       <div className={styles.app}>
-        <div className="panel-default">Home Now!</div>
+        <div>Home Now!</div>
       </div>
     );
   }
